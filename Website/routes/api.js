de22d1aaf1bb4f15d02c615f5c2fd11175e98405
Brian Olencki (bolencki13) <bolencki13@gmail.com>
2017-03-10 21:23:02 -0500

    Initial Database implementation

--- Website/routes/api.js
+++ Website/routes/api.js
@@ -6,6 +6,13 @@ router.use(function(req, res, next) {
     next();
 });
 
+// get user (user_id) (/user_id)
+// get achievement (user_id => achievements to user || all)
+
+// post user (login)
+// post doctor (login)
+// post drags (drag_id,duration,date) (create)
+// achievements calculated server side
 
 
 module.exports = router;
