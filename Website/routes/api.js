b7b90ad0f5a331d83d21c074b1f86c737995b271
Brian Olencki (bolencki13) <bolencki13@gmail.com>
2017-03-11 11:32:14 -0500

    Initial patients implementation (untested)

--- Website/routes/api.js
+++ Website/routes/api.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+var patients = require('./../functions/patientManagement');
+
 // MIDDLEWARE
 router.use(function(req, res, next) {
     next();
 });
 
-// get user (user_id) (/user_id)
+
+// OVERVIEW
+router.get('/', function(req, res) {
+    var json = {};
+    json["result"] = "success";
+
+    var aryURLs = new Array();
+    for (var x = 0; x < router.stack.length; x++) {
+        if (router.stack[x].route && router.stack[x].route.path && router.stack[x].route.path != "/") {
+            aryURLs.push(router.stack[x].route.path);
+        }
+    }
+    json["endpoints"] = aryURLs;
+    res.send(json);
+});
+
+// PATIENTS
+router.post('/patients/register', function(req, res) {
+    patients.register(req, function(error, json) {
+        if (error) console.log("/patients/register/ => " + error);
+        res.send(json);
+    });
+});
+router.get('/patients/info/:patientID', function(req, res) {
+    patients.info(req.params.patientID, function(error, json) {
+        if (error) console.log("/patients/info/" + req.params.patientID + " => " + error);
+        res.send(json);
+    });
+});
 // get achievement (user_id => achievements to user || all)
 
 // post user (login)
