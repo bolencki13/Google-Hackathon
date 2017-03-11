c064343be7e7f3b3418c30b94d3143add4e8aefa
Andrew Boryk <aboryk@mercymavericks.edu>
2017-03-10 19:27:28 -0500

    Updated to add objects

---
+++ iOS/tobaccno/TBNDevice.h
@@ -0,0 +1,13 @@
+//
+//  TBNDevice.h
+//  tobaccno
+//
+//  Created by Andrew Boryk on 3/10/17.
+//  Copyright © 2017 Brian Olencki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBNDevice : NSObject
+
+@end
